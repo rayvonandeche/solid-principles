35832cee0bd92f3a085a0ac480f8309bf4f0c9cf
Rayvon <158500435+rayvonandeche@users.noreply.github.com>
2025-06-12 08:57:38 +0300

    Create DependencyInversionPrinciple.java

---
+++ DependencyInversionPrinciple.java
@@ -0,0 +1,23 @@
+// Dependency Inversion Principle (DIP)
+// High-level modules should not depend on low-level modules directly.
+// In this example the databse can be swaaped without modifiying thr Application
+
+interface Database {
+  void connect()
+}
+
+class  MySqlDatabase implements Database {
+  public  void connect(){
+    System.out.println("Connecting to MySQL Database...");
+  }
+}
+
+class Application{
+  private Database database;
+
+Application(Database database){
+  this.database = database;
+}
+  void run(){
+    database.connect();
+}
